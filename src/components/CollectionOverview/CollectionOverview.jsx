b0156a41562509a5d12c03de23751a857491d1e3
Alfio Martini <alfio.martini@gmail.com>
2021-08-26 12:28:54 -0300

    Fixed CollectionOverview (SHOPDATA is a map)

--- src/components/CollectionOverview/CollectionOverview.jsx
+++ src/components/CollectionOverview/CollectionOverview.jsx
@@ -6,12 +6,15 @@ import CollectionPreview from '../CollectionPreview/CollectionPreview';
 import {selectCollections} from '../../reducers/selectors';
 
 const CollectionOverview = ({shopCollection}) =>{
+  const collectionKeys = Object.keys(shopCollection);
+  console.log('collectionKeys', collectionKeys);
   return(
     <div className='collection-overview'>
       {
-        shopCollection.map(itemType => (
-          <CollectionPreview key={itemType.id} itemType={itemType} size={4}/>
-        ))
+        collectionKeys.map(key => {
+          const itemType = shopCollection[key];
+          return <CollectionPreview key={itemType.id} itemType={itemType} size={4}/>
+        })
       }
     </div>
   )
