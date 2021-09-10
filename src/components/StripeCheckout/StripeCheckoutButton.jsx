52d0a51b03d1adbb293b5b98b23b7b900870ceab
Alfio Martini <alfio.martini@gmail.com>
2021-09-09 21:34:24 -0300

    Compiled a new build version for heroku deployment

--- src/components/StripeCheckout/StripeCheckoutButton.jsx
+++ src/components/StripeCheckout/StripeCheckoutButton.jsx
@@ -13,7 +13,7 @@ const StripeCheckoutButton = ({price}) =>{
   return (
     <StripeCheckout 
       label = 'Pay Now'
-      name = 'CRWN Clothing Ltd.'
+      name = 'Clothing Shop Ltd.'
       billingAddress
       shippingAddress
       description = {`Your total price is $${price}`}
